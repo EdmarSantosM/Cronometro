0c9d04e1e7e9371d409d24d4fcfba60c7df05062
Edmar Santos <santosem4@hotmail.com>
2022-03-27 22:01:41 -0300

    Definindo multiplicador
    Calcula  o valor com base  em 60.
    time  = 0  sair  da aplicação
    
    Chamada  do método ( time * multiplier )

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -25,9 +25,20 @@ namespace StopWatch
             char type = char.Parse(data.Substring(data.Length - 1,1));
             int time = int.Parse(data.Substring(0,data.Length - 1));
 
-            System.Console.WriteLine(data);
-            System.Console.WriteLine(type);
-             System.Console.WriteLine(time);
+           int multiplier = 1;
+
+           if(type == 'm')
+            multiplier = 60;
+
+            if ( time == 0)
+                System.Environment.Exit(0);
+
+                Start(time * multiplier);
+
+
+                
+
+          
         }
 
         static void Start( int time)
